f06d8bbca2ac08830a5a91706ee069e52490336a
antea-j <63292755+antea-j@users.noreply.github.com>
2020-04-14 15:06:11 +0200

    Add files via upload
    Podaci u SPSS, faktorska analiza u R

---
+++ FA - istrazivanje A.R
@@ -0,0 +1,35 @@
+
+library(foreign)
+library(GPArotation)
+library(foreign)
+library(corrplot)
+library(FactoMineR)
+library(factoextra)
+
+
+choose.files()
+podaci <- as.data.frame(read.spss("C:\\ISTR.A.sav"))
+attach(podaci)
+
+
+sum(is.na(podaci))
+cisti_podaci <- podaci[complete.cases(podaci),]
+
+
+matrix_podaci <- data.matrix(cisti_podaci, rownames.force = NA)
+print(matrix_podaci)
+summary(matrix_podaci)
+
+cor_podaci <- cor(matrix_podaci)
+print(cor_podaci)
+
+fa_podaci <- fa(cor_podaci, nfactors = 6)
+summary(fa_podaci)
+
+
+
+
+
+
+
+
---
+++ FA - stavovi o ogovaranju i rodni identitet A.R
@@ -0,0 +1,63 @@
+# FAKTORSKA ANALIZA - rodni identitet i ogovaranje #
+
+library(psych)
+library(GPArotation)
+library(foreign)
+library(corrplot)
+library(FactoMineR)
+library(factoextra)
+
+# podaci
+file.choose()
+podaci <- as.data.frame(read.spss("C:\\O.R.I.-A.sav"))
+attach(podaci)
+
+
+head(podaci,10)
+
+# 0 NA elemenata
+sum(is.na(podaci))
+cisti_podaci <- podaci[complete.cases(podaci),]
+
+# matrix
+matrix_podaci <- data.matrix(cisti_podaci, rownames.force = NA)
+print(matrix_podaci)
+summary(matrix_podaci)
+
+# korelacijska tablica
+cor_podaci <- cor(matrix_podaci)
+
+# FA
+fa_podaci <- fa(cor_podaci, nfactors = 6)
+summary(fa_podaci)
+
+#ili
+str(cisti_podaci)
+podaci_fa <- FAMD(cisti_podaci, graph = FALSE)
+print(podaci_fa)
+
+# vizulizacija
+fviz_screeplot(podaci_fa)
+fviz_famd_var(podaci_fa, repel = TRUE)
+fviz_contrib(podaci_fa, "var", axes = 1)
+
+# analiza var
+varijable <- get_famd_var(podaci_fa)
+varijable
+head(varijable$contrib)
+head(varijable$cos2)
+head(varijable$coord)
+varijable$contrib
+
+# analiza kval var
+kval <- get_famd_var(podaci_fa, "quali.var")
+kval
+
+# analiza kvant
+kvant <- get_famd_var(podaci_fa, "quanti.var")
+head(kvant)
+
+
+
+
+
